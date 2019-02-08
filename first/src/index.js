dc17b57733e6559a538eda67f5e17bdc0f436cf8
Roman <r2598509@gmail.com>
2019-02-08 16:45:14 +0300

    lesson 14 ended

--- first/src/index.js
+++ first/src/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
+
+const shape = {
+    type: "segment",
+    cordinates: {
+        start: [10, 15],
+        end: [17, 15]
+    }
+};
+
+const { cordinates: {start: [startX, startY], end: [endX, endY]} } = shape;
+
+console.log(startX, startY, endX, endY);
+
+
+
 function TodoItem() {
     return (
         <span>Drink Coffee</span>
     );
 }
 
-ReactDOM.render(<TodoItem/>, document.getElementById('root'));
+function TodoList() {
+    return (
+        <ul>
+            <li><TodoItem/></li>
+            <li><TodoItem/></li>
+            <li><TodoItem/></li>
+        </ul>
+    );
+}
+
+ReactDOM.render(<TodoList/>, document.getElementById('root'));
