f18dc4d4de8833e532833b5c6e6e239382770728
unknown <r2598509@gmail.com>
2019-12-11 00:01:28 +0300

    run app with ReactDOM webpack+babel

--- own_app/src/index.js
+++ own_app/src/index.js
@@ -1,7 +1,20 @@
-import Log from './log';
+/* import Log from './log';
 import Calc from './calc';
 
 const calc = new Calc();
 const log = new Log();
 
-log.log(calc.add(1, 2, 3));
+log.log(calc.add(1, 2, 3)); */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const el = <h1>Hello!</h1>;
+console.log(el);
+console.dir(el);
+console.log('-------------');
+
+
+
+ReactDOM.render(el, document.getElementById('root'));
+console.log(document.querySelector('h1'));
+console.dir(document.querySelector('h1'));
